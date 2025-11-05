c18d471cde59e32cc56d65631117cd1caffa9af7
Dawid Mądrzyk <maddawid@gmail.com>
2025-11-05 01:14:26 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [currPage, setcurrPage] = useState('homes')
 
   return (
     <>
-      <div className="headerContent">
-        <h1>Dawid Mądrzyk</h1>
-      </div>
-      <div className="bodyContent"></div>
+      <Navbar />
+      {currPage === 'home' && <Home />}
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className="headerContent">
+            <h1>Dawid Mądrzyk</h1>
+        </div>
+    );
+}
